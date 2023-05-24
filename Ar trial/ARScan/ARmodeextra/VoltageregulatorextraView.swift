7d597062075780c4779b36c1513d16320e4735bf
hhxhans <213192693@seu.edu.cn>
2023-05-24 19:17:26 +0800

    add circuit image

---
+++ Ar trial/ARScan/ARmodeextra/VoltageregulatorextraView.swift
@@ -0,0 +1,20 @@
+//
+//  VoltageregulatorextraView.swift
+//  Ar trial
+//
+//  Created by niudan on 2023/5/24.
+//
+
+import SwiftUI
+
+struct VoltageregulatorextraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VoltageregulatorextraView_Previews: PreviewProvider {
+    static var previews: some View {
+        VoltageregulatorextraView()
+    }
+}
